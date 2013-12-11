c8e882ac1c37e5aabbcc0bea30ff97be6f88c881
Andreas Rönnquist <gusnan@gusnan.se>
2013-12-11 10:29:50 +0100

    Lägg till uppgiftsfrågan

--- labb2/uppg1.sh
+++ labb2/uppg1.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#Skriv ett skript som skriver ut ”Hej Johan, du befinner dig i katalogen: 
+# /home/Johan.” där Johan är användarnamnet på inloggad användare och: 
+# /home/Johan är den katalog användaren befinner sig i när han kör skriptet.
+
 
 echo "Hej $USER, du befinner dig i katalogen $PWD.";
-
